e83302c4b10b8533f5f6a822fb2aa817b276b2b1
Haydn Joel Gately <h.j.gately-14@student.lboro.ac.uk>
2020-06-26 08:58:56 +0100

    Initial Commit

---
+++ FootballPassport/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FootballPassport
+//
+//  Created by Haydn Joel Gately on 26/06/2020.
+//  Copyright © 2020 Haydn Gately. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
